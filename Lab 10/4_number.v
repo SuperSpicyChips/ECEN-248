64e4f5f356570f27c20a2c8a4574d98637ad9d49
SuperSpicyChips <89935715+SuperSpicyChips@users.noreply.github.com>
2024-11-19 15:10:02 -0600

    Update 4_number.v

--- Lab 10/4_number.v
+++ Lab 10/4_number.v
@@ -2,10 +2,10 @@
 
 module combination_lock_4(
     //instantiating outputs and inputs
-    output reg [1:0] state,
+    output reg [2:0] state,
     output wire [3:0] Lock,  
-    input wire Key0, //Unlock button 1
-    input wire Key1, //Unlock button 2
+    input wire Key1, //Unlock button 1
+    input wire Key2, //Unlock button 2
     input wire [3:0] Password, //INdicate number
     input wire Reset, //Reset
     input wire Clk //Clock
@@ -20,48 +20,54 @@ module combination_lock_4(
     parameter S2 = 3'b010;
     parameter S3 = 3'b011;
     parameter S4 = 3'b100;
-    
+
     always @(*) //Always
+        if (Reset) //If reset is ever pressed it goes to 0
+            nextState = S0;
+        else
+            
         case(state)
             S0:begin //State 0
-               if (Password == 4'b1101 && Key0) //If password is 13 and button 1 is being pressed
+                if (Password == 4'b1101 && Key1) //If password is 13 and button 1 is being pressed
                    nextState = S1; //Go to next state
                else
                    nextState = S0; //Stay at same state
                end
             S1:begin  //State 1
-                if (Password == 4'b0111 && Key1) //If password is 7 and button 2 is pressed
+                if (Password == 4'b0111 && Key2) //If password is 7 and button 2 is pressed
                     nextState = S2; //Go to next state
-                else
+                else if (Password != 4'b0111 && Key2)
                     nextState = S0; //Reset to beginning
+                else
+                    nextState = S1; //Stay at current state
                 end
             S2:begin //state 2
-                if (Password == 4'b1001 && Key0) //If password is 9 and button 1 is pressed
+                if (Password == 4'b1001 && Key1) //If password is 9 and button 1 is pressed
                     nextState = S3; //Go top next state
+                else if (Password != 4'b1001 && Key1)
+                    nextState = S0; //Reset to beginning
                 else
-                    nextState = S0; //Reset
+                    nextState = S2; //Stay at current state
                 end
-            S3:begin //State 4
-                if (Password == 4'b1011 && Key1) //If password is 11 and button 2 is pressed
-                    nextState = S0; //If reset is pressed, goes back to initial state
+            S3:begin //State 3
+                if (Password == 4'b0011 && Key1) //If password is 11 and button 2 is pressed
+                    nextState = S4; //If reset is pressed, goes back to initial state
+                else if (Password != 4'b0111 && Key2)
+                    nextState = S0; //Reset to beginning
                 else
-                    nextState = S4; //Else stays at state 3
+                    nextState = S3; //Stay at current state
                 end
             S4:begin //State 4
-                if(Reset == 1)
-                    nextState = S0; //If reset is pressed, goes back to initial state
-                else
-                    nextState = S4; //Else stays at state 4
+                nextState = S4;
                 end
          endcase
          
      always@(posedge Clk) //Whenever at Clk = 1
-        if(Reset)
-            state <= S0;  //If reset then State = S0
-        else
-            state <= nextState; //Else state = nextState
+        begin
+            state = nextState
+        end
             
-     assign Lock = (state == S4) ? 4'b1111 : 4'b0000; //If state = S4 lock = 1111 else lock = 0000
+    assign Lock = (state == S4) ? 4'b1111 : (state == S3) ? 4'b0111 : (state == S2) ? 4'b0011 : (state == S1) ? 4'b0001 : 4'b0000; //If state = S4 lock = 1111 else lock = 0000
             
 endmodule
 
