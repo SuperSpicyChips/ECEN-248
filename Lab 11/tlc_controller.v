ef006f0f00fc45cafef39dd4dcd874beadabc04e
SuperSpicyChips <89935715+SuperSpicyChips@users.noreply.github.com>
2024-11-19 22:21:34 -0600

    Add files via upload

--- Lab 11/tlc_controller.v
+++ Lab 11/tlc_controller.v
@@ -1,26 +1,41 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/19/2024 10:03:12 PM
-// Design Name: 
-// Module Name: tlc_controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+`default_nettype none
 
 module tlc_controller(
-
+    //instantiate input and ouptut
+    output wire [1:0] highwaySignal, farmSignal,
+    output wire [3:0] JB,
+    input wire Clk,
+    input wire Rst,
+    input wire farmSensor
     );
+    
+    //intermediate nets\
+    wire RstSync;
+    wire RstCount;
+    reg [30:0] Count;
+    
+    assign JB[3] = RstCount;
+    
+    //synchronize button inputs
+    synchronizer syncRst(RstSync, Rst, Clk);
+    
+    //instantiate FSM
+    tlc_fsm FSM(
+            .state(JB[2:0]),
+            .Clk(Clk),
+            .Rst(RstSync),
+            .highwaySignal(highwaySignal),
+            .farmSignal(farmSignal),
+            .RstCount(RstCount),
+            .Count(Count),
+            .farmSensor(farmSensor)
+    );
+    
+    // counter
+    always@(posedge Clk)
+        if(Rst) //reset
+            Count = 0;
+        else
+            Count = Count + 1; //add 1
 endmodule
